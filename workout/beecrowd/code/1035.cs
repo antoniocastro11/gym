7f00741fd5e7ef2f014888b134a403e6a1988a52
Antonio Castro <antoniog_castro@hotmail.com>
2026-05-21 09:07:40 -0400

    feat: primeira versao 1035 bee

---
+++ workout/beecrowd/code/1035.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class URI
+{
+    static void Main(string[] args)
+    {
+        string entrada = Console.ReadLine();
+        string[] numeros = entrada.Split(' ');
+
+        int a = int.Parse(numeros[0]);
+        int b = int.Parse(numeros[1]);
+        int c = int.Parse(numeros[2]);
+        int d = int.Parse(numeros[3]);
+
+        if (b > c && d > a && c + d > a + b && c > 0 && d > 0 && a % 2 = 0)
+        {
+            Console.WriteLine("Valores aceitos");
+        }
+        else
+            Console.WriteLine("Valores nao aceitos");
+    }
+}
